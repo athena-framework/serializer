888ec7176d2cc42a1d2e76ee7167fd4537274def
George Dietrich <george@dietrich.app>
2020-05-03 21:21:08 -0400

    Support discriminators

--- src/navigators/deserialization_navigator.cr
+++ src/navigators/deserialization_navigator.cr
@@ -1,6 +1,6 @@
 module Athena::Serializer::Navigators::DeserializationNavigatorInterface
   abstract def accept(type : ASR::Serializable.class, data : ASR::Any)
-  abstract def accept(type : _, data : ASR::Any)
+  abstract def accept(type : T, data : ASR::Any) forall T
 end
 
 struct Athena::Serializer::Navigators::DeserializationNavigator
@@ -10,24 +10,38 @@ struct Athena::Serializer::Navigators::DeserializationNavigator
 
   def initialize(@visitor : ASR::Visitors::DeserializationVisitorInterface, @context : ASR::DeserializationContext); end
 
-  def accept(type : ASR::Serializable.class, data : ASR::Any)
-    properties = type.deserialization_properties
+  def accept(type : T.class, data : ASR::Any) forall T
+    {% unless T.instance <= ASR::Serializable %}
+      {% if T.has_method? :deserialize %}
+        @visitor.visit type, data
+      {% end %}
+    {% else %}
+      {% if ann = T.instance.annotation(ASR::Discriminator) %}
+        if key = data[{{ann[:key]}}]?
+          type = case key
+            {% for k, t in ann[:map] %}
+              when {{k}} then {{t}}
+            {% end %}
+          else
+            raise "Unknown key"
+          end
+        else
+          raise "Missing discriminator key"
+        end
+      {% end %}
 
-    # Apply exclusion strategies if one is defined
-    if strategy = @context.exclusion_strategy
-      properties.reject! { |property| strategy.skip_property? property, @context }
-    end
+      properties = type.deserialization_properties
 
-    object = @object_constructor.construct self, properties, data, type
+      # Apply exclusion strategies if one is defined
+      if strategy = @context.exclusion_strategy
+        properties.reject! { |property| strategy.skip_property? property, @context }
+      end
 
-    object.run_postdeserialize
+      object = @object_constructor.construct self, properties, data, type
 
-    object
-  end
+      object.run_postdeserialize
 
-  def accept(type : T, data : ASR::Any) forall T
-    {% if T.has_method? :deserialize %}
-      @visitor.visit type, data
+      object
     {% end %}
   end
 end
