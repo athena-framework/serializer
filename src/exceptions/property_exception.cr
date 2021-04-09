a57d97183de211d30e5418c4ee2d8f85ccbb3669
George Dietrich <george@dietrich.app>
2021-04-09 16:07:15 -0400

    Subclass property specific exceptions under deserialization ex

--- src/exceptions/property_exception.cr
+++ src/exceptions/property_exception.cr
@@ -3,7 +3,7 @@ require "./serializer_exception"
 # Represents an error due to an invalid property.
 #
 # Exposes the property's name.
-class Athena::Serializer::Exceptions::PropertyException < Athena::Serializer::Exceptions::SerializerException
+class Athena::Serializer::Exceptions::PropertyException < Athena::Serializer::Exceptions::DeserializationException
   getter property_name : String
 
   def initialize(message : String, @property_name : String)
